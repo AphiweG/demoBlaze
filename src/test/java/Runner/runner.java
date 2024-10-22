bd9a923c9d5544970aa4d24572edc24fa4dc4ad6
AphiweG <49280947+AphiweG@users.noreply.github.com>
2024-10-22 10:38:01 +0200

    Added allure reports and screenshots

--- src/test/java/Runner/runner.java
+++ src/test/java/Runner/runner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(features = "src/test/resources/Features",
         glue = {"Steps"},
         tags = "@Deposit or @Deposits or @Transaction",
-        plugin = {"pretty", "html:target/cucumber-reports.html"},
+        plugin = {"io.qameta.allure.cucumber5junit.AllureCucumber5JUnit"},
         publish = true)
 public class runner extends AbstractTestNGCucumberTests {
 }
---
+++ src/test/java/Utils/Screenshots.java
@@ -0,0 +1,19 @@
+package Utils;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class Screenshots {
+    WebDriver driver;
+
+    @After
+    public void takeScreenshot(Scenario scenario){
+        if (scenario.isFailed()){
+            byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+        }
+    }
+}
